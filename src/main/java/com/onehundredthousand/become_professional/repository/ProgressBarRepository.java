2c6302eadfe8d73510d328c25acc8117d123f4f6
kosenkovps <kosenkovps@ntk.novotelecom.ru>
2023-01-06 00:37:25 +0700

    PG-1:ADD:build simple logic

---
+++ src/main/java/com/onehundredthousand/become_professional/repository/ProgressBarRepository.java
@@ -0,0 +1,8 @@
+package com.onehundredthousand.become_professional.repository;
+
+import com.onehundredthousand.become_professional.model.ProgressBar;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProgressBarRepository extends CrudRepository<ProgressBar, String> {
+
+}
